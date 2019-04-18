ee57214579ec4a4f8a39cfd81991ac376227056c
Vasile <vgrinciuc@straigherline.com>
2019-04-18 15:06:49 +0300

    Base config for webpack-dev-server

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Init Project
 ```sh
     yarn add webpack webpack-cli
 ```
+
+## Webpack Dev Server
+
+```sh
+    yarn add webpack-dev-server html-webpack-plugin
+```
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  }
+    filename: '[chunkhash].js'
+  },
+  devServer:{
+    open:true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:path.resolve(__dirname, 'public/index.html')
+    })
+  ]
 };
